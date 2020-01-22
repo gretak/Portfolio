b6f2179aa7c3fe85faaaaebac5c2c808be73384e
Greta Kavaliauskaite <kavaliauskaite3@gmail.com>
2020-01-22 20:46:02 +0000

    added cv download axios set up

--- src/App.vue
+++ src/App.vue
@@ -105,9 +105,9 @@
               medium
               class="ma-3 teal--text"
               color="white"
-              href="./static/cv.pdf"
+              v-on:click="downloadItem('./gretak-cv.pdf')"
             >
-              CV
+              download CV
             </v-btn>
 
             <v-btn
@@ -191,18 +191,34 @@
 
 <script>
 import CardsList from "./components/CardsList";
-//import pdf from "vue-pdf";
+import axios from "axios";
 
 export default {
   name: "App",
 
   components: {
     CardsList
-    //pdf
   },
 
   data: () => ({
     //
-  })
+  }),
+
+  methods: {
+    downloadItem(fileName) {
+      axios({
+        url: fileName,
+        method: "GET",
+        responseType: "blob" // important
+      }).then(response => {
+        const url = window.URL.createObjectURL(new Blob([response.data]));
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute("download", fileName);
+        document.body.appendChild(link);
+        link.click();
+      });
+    }
+  }
 };
 </script>
